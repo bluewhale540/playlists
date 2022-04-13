049ed97ddc327c3adfe5f6ea294456254c31f4ab
bluewhale540 <mathewcpham@gmail.com>
2022-04-12 20:01:12 -0400

    homepage and ui stuff

--- php_playlists/homepage.php
+++ php_playlists/homepage.php
@@ -24,7 +24,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <!--Navbar-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">project-b11</a>
+        <a class="navbar-brand" href="/">Title</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
